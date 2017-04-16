f317c1204e429a4c1952aa4e3f42679750ad2903
cerkit <mearls@hotmail.com>
2017-04-16 03:57:51 -0500

    initial commit

---
+++ raspi-midi-uwp/MainPage.xaml.cs
@@ -0,0 +1,386 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+using Windows.Storage.Streams;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RaspiMidiUwp
+{
+    public sealed partial class MainPage : Page
+    {
+        private const int BAUD_RATE = 115200;
+
+        /// <summary>
+        /// Private variables
+        /// </summary>
+        private SerialDevice serialPort = null;
+        DataWriter dataWriteObject = null;
+        DataReader dataReaderObject = null;
+
+        private ObservableCollection<DeviceInformation> listOfDevices;
+        private CancellationTokenSource ReadCancellationTokenSource;
+        private DeviceInformation _defaultDevice;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            comPortInput.IsEnabled = false;
+            sendNoteButton.IsEnabled = false;
+            listOfDevices = new ObservableCollection<DeviceInformation>();
+
+            SetupPage();
+        }
+
+        private async void SetupPage()
+        {
+            string aqs = SerialDevice.GetDeviceSelector();
+            var dis = await DeviceInformation.FindAllAsync(aqs);
+
+            _defaultDevice = dis[0];
+
+            serialPort = await SerialDevice.FromIdAsync(_defaultDevice.Id);
+            if (serialPort == null) return;
+
+            // Disable the 'Connect' button 
+            comPortInput.IsEnabled = false;
+
+            try
+            {
+                
+                if (serialPort == null) return;
+
+                // Disable the 'Connect' button 
+                comPortInput.IsEnabled = false;
+
+                // Configure serial settings
+                serialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+                serialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+                serialPort.BaudRate = BAUD_RATE;
+                serialPort.Parity = SerialParity.None;
+                serialPort.StopBits = SerialStopBitCount.One;
+                serialPort.DataBits = 8;
+                serialPort.Handshake = SerialHandshake.None;
+
+                // Display configured settings
+                status.Text = "Serial port configured successfully: ";
+                status.Text += serialPort.BaudRate + "-";
+                status.Text += serialPort.DataBits + "-";
+                status.Text += serialPort.Parity.ToString() + "-";
+                status.Text += serialPort.StopBits;
+
+                // Set the RcvdText field to invoke the TextChanged callback
+                // The callback launches an async Read task to wait for data
+                rcvdText.Text = "Waiting for data...";
+
+                // Create cancellation token object to close I/O operations when closing the device
+                ReadCancellationTokenSource = new CancellationTokenSource();
+
+                // Enable 'WRITE' button to allow sending data
+                sendNoteButton.IsEnabled = true;
+
+                Listen();
+
+            }
+            catch (Exception ex)
+            {
+                status.Text = ex.Message;
+                comPortInput.IsEnabled = true;
+                sendNoteButton.IsEnabled = false;
+            }
+        }
+
+        /// <summary>
+        /// comPortInput_Click: Action to take when 'Connect' button is clicked
+        /// - Get the selected device index and use Id to create the SerialDevice object
+        /// - Configure default settings for the serial port
+        /// - Create the ReadCancellationTokenSource token
+        /// - Start listening on the serial port input
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void comPortInput_Click(object sender, RoutedEventArgs e)
+        {
+            await ConnectToCOMPort();
+        }
+
+        private async Task ConnectToCOMPort()
+        {
+            DeviceInformation entry = _defaultDevice;
+
+            try
+            {
+                serialPort = await SerialDevice.FromIdAsync(entry.Id);
+                if (serialPort == null) return;
+
+                // Disable the 'Connect' button 
+                comPortInput.IsEnabled = false;
+
+
+                // Configure serial settings
+                serialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+                serialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+                serialPort.BaudRate = 57600;
+                serialPort.Parity = SerialParity.None;
+                serialPort.StopBits = SerialStopBitCount.One;
+                serialPort.DataBits = 8;
+                serialPort.Handshake = SerialHandshake.None;
+
+                // Display configured settings
+                status.Text = "Serial port configured successfully: ";
+                status.Text += serialPort.BaudRate + "-";
+                status.Text += serialPort.DataBits + "-";
+                status.Text += serialPort.Parity.ToString() + "-";
+                status.Text += serialPort.StopBits;
+
+                // Set the RcvdText field to invoke the TextChanged callback
+                // The callback launches an async Read task to wait for data
+                rcvdText.Text = "Waiting for data...";
+
+                // Create cancellation token object to close I/O operations when closing the device
+                ReadCancellationTokenSource = new CancellationTokenSource();
+
+                // Enable 'WRITE' button to allow sending data
+                sendNoteButton.IsEnabled = true;
+
+                Listen();
+            }
+            catch (Exception ex)
+            {
+                status.Text = ex.Message;
+                comPortInput.IsEnabled = true;
+                sendNoteButton.IsEnabled = false;
+            }
+        }
+
+        /// <summary>
+        /// sendTextButton_Click: Action to take when 'WRITE' button is clicked
+        /// - Create a DataWriter object with the OutputStream of the SerialDevice
+        /// - Create an async task that performs the write operation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void sendNoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (serialPort != null)
+                {
+                    // Create the DataWriter object and attach to OutputStream
+                    dataWriteObject = new DataWriter(serialPort.OutputStream);
+
+                    ////Launch the WriteAsync task to perform the write
+                    //await WriteAsync();
+                    SendNote(Convert.ToUInt32(txtNote.Text), Convert.ToUInt32(txtVelocity.Text), Convert.ToUInt32(txtChannel.Text));
+                }
+                else
+                {
+                    status.Text = "No default device found";
+                }
+            }
+            catch (Exception ex)
+            {
+                status.Text = this.Name + ": " + ex.Message;
+            }
+            finally
+            {
+                // Cleanup once complete
+                if (dataWriteObject != null)
+                {
+                    dataWriteObject.DetachStream();
+                    dataWriteObject = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// WriteAsync: Task that asynchronously writes data from the input text box 'sendText' to the OutputStream 
+        /// </summary>
+        /// <returns></returns>
+        private async Task WriteAsync()
+        {
+            Task<UInt32> storeAsyncTask;
+
+            byte[] bytes = { };
+
+            // loop through and send the byte values of our message
+            dataWriteObject.ByteOrder = ByteOrder.BigEndian;
+
+            dataWriteObject.WriteByte(Convert.ToByte(36)); // note (60 = middle c)
+            dataWriteObject.WriteByte(Convert.ToByte(127)); // velocity (127 = loudest)
+            dataWriteObject.WriteByte(Convert.ToByte(144)); // note on channel 0
+
+            // Launch an async task to complete the write operation
+            storeAsyncTask = dataWriteObject.StoreAsync().AsTask();
+
+            UInt32 bytesWritten = await storeAsyncTask;
+            if (bytesWritten > 0)
+            {
+                //status.Text = sendText.Text + ", ";
+                //status.Text += "bytes written successfully!";
+            }
+        }
+
+        private void SendNote(uint note, uint velocity, uint channel, bool onOrOff = true)
+        {
+            dataWriteObject.ByteOrder = ByteOrder.BigEndian;
+
+            dataWriteObject.WriteByte(Convert.ToByte(note));
+            dataWriteObject.WriteByte(Convert.ToByte(velocity));
+            dataWriteObject.WriteByte(Convert.ToByte(channel));
+
+            // Launch an async task synchronously so we don't play one note before another
+            dataWriteObject.StoreAsync().AsTask().Wait();
+        }
+
+        /// <summary>
+        /// - Create a DataReader object
+        /// - Create an async task to read from the SerialDevice InputStream
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Listen()
+        {
+            try
+            {
+                if (serialPort != null)
+                {
+                    dataReaderObject = new DataReader(serialPort.InputStream);
+
+                    // keep reading the serial input
+                    while (true)
+                    {
+                        await ReadAsync(ReadCancellationTokenSource.Token);
+                    }
+                }
+            }
+            catch (TaskCanceledException tce)
+            {
+                status.Text = "Reading task was cancelled, closing device and cleaning up";
+                CloseDevice();
+            }
+            catch (Exception ex)
+            {
+                status.Text = ex.Message;
+            }
+            finally
+            {
+                // Cleanup once complete
+                if (dataReaderObject != null)
+                {
+                    dataReaderObject.DetachStream();
+                    dataReaderObject = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// ReadAsync: Task that waits on data and reads asynchronously from the serial device InputStream
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        private async Task ReadAsync(CancellationToken cancellationToken)
+        {
+            Task<UInt32> loadAsyncTask;
+
+            uint ReadBufferLength = 1024;
+
+            // If task cancellation was requested, comply
+            cancellationToken.ThrowIfCancellationRequested();
+
+            // Set InputStreamOptions to complete the asynchronous read operation when one or more bytes is available
+            dataReaderObject.InputStreamOptions = InputStreamOptions.Partial;
+
+            using (var childCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken))
+            {
+                // Create a task object to wait for data on the serialPort.InputStream
+                loadAsyncTask = dataReaderObject.LoadAsync(ReadBufferLength).AsTask(childCancellationTokenSource.Token);
+
+                // Launch the task and wait
+                UInt32 bytesRead = await loadAsyncTask;
+                if (bytesRead > 0)
+                {
+                    rcvdText.Text = string.Empty;
+
+                    //rcvdText.Text = dataReaderObject.ReadString(bytesRead);
+                    byte[] bytes = { };
+                    var byteOrder = dataReaderObject.ByteOrder;
+
+                    // red them in reverse since it's BigEndian
+                    for (uint i = dataReaderObject.UnconsumedBufferLength; i > 0; i--)
+                    {
+                        rcvdText.Text += " " + dataReaderObject.ReadByte().ToString();
+                    }
+                    //rcvdText.Text = string.Format("byte count: {0}\r\n", bytes.Length);
+                    //foreach(var b in bytes)
+                    //{
+                    //    rcvdText.Text += Convert.ToString(b);
+                    //}
+
+                    status.Text = "bytes read successfully!";
+                }
+            }
+        }
+
+        /// <summary>
+        /// CancelReadTask:
+        /// - Uses the ReadCancellationTokenSource to cancel read operations
+        /// </summary>
+        private void CancelReadTask()
+        {
+            if (ReadCancellationTokenSource != null)
+            {
+                if (!ReadCancellationTokenSource.IsCancellationRequested)
+                {
+                    ReadCancellationTokenSource.Cancel();
+                }
+            }
+        }
+
+        /// <summary>
+        /// CloseDevice:
+        /// - Disposes SerialDevice object
+        /// - Clears the enumerated device Id list
+        /// </summary>
+        private void CloseDevice()
+        {
+            if (serialPort != null)
+            {
+                serialPort.Dispose();
+            }
+            serialPort = null;
+
+            comPortInput.IsEnabled = true;
+            sendNoteButton.IsEnabled = false;
+            rcvdText.Text = "";
+            listOfDevices.Clear();
+        }
+
+        /// <summary>
+        /// closeDevice_Click: Action to take when 'Disconnect and Refresh List' is clicked on
+        /// - Cancel all read operations
+        /// - Close and dispose the SerialDevice object
+        /// - Enumerate connected devices
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void closeDevice_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                status.Text = "";
+                CancelReadTask();
+                CloseDevice();
+            }
+            catch (Exception ex)
+            {
+                status.Text = ex.Message;
+            }
+        }
+    }
+}
