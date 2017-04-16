86d4d455cc626f330eeadf2cde1578078e3d456a
Michael Earls <mearls@hotmail.com>
2017-04-16 18:10:43 -0400

    Pi interrupts (#1)
    * Added Adafruit Windows IoT library
    
    * Added MCP23017 polling and IObservable with an event on the observer.
    
    * failed attempt at observable pattern. Something is wrong with the timer.
    Removed Wait() for send note and added note off.
    
    * Failed attempt at using MCP23017. There is currently no easy way to get interrupts from the inputs with the Adafruit library.
    
    * This entire approach is faulty. Windows 10 and .NET are too much of a burden for the OS to receive and process MIDI messages effectively.

---
+++ raspi-midi-uwp/Classes/MidiMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaspiMidiUwp.Classes
+{
+    public class MidiMessage
+    {
+        public uint MsgChannel { get; set; }
+        public uint Velocity { get; set; }
+        public uint Note { get; set; }
+    }
+}
--- raspi-midi-uwp/MainPage.xaml.cs
+++ raspi-midi-uwp/MainPage.xaml.cs
@@ -9,23 +9,33 @@ using Windows.Devices.SerialCommunication;
 using Windows.Storage.Streams;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using Windows.System.Threading;
+using Windows.Devices.Gpio;
+using RaspiMidiUwp.Classes;
+using System.Collections.Generic;
 
 namespace RaspiMidiUwp
 {
     public sealed partial class MainPage : Page
     {
         private const int BAUD_RATE = 115200;
+        private const ushort PIN_MASK = 65534;
+        private const uint MIDI_NOTE_ON = 144;
+        private const uint MIDI_NOTE_OFF = 128;
+        private Queue<MidiMessage> msgQueue;
+        private DispatcherTimer timer;
 
         /// <summary>
         /// Private variables
         /// </summary>
         private SerialDevice serialPort = null;
-        DataWriter dataWriteObject = null;
-        DataReader dataReaderObject = null;
 
         private ObservableCollection<DeviceInformation> listOfDevices;
         private CancellationTokenSource ReadCancellationTokenSource;
         private DeviceInformation _defaultDevice;
+
+        private GpioController _gpio;
 
         public MainPage()
         {
@@ -44,48 +54,25 @@ namespace RaspiMidiUwp
 
             _defaultDevice = dis[0];
 
-            serialPort = await SerialDevice.FromIdAsync(_defaultDevice.Id);
-            if (serialPort == null) return;
-
             // Disable the 'Connect' button 
             comPortInput.IsEnabled = false;
 
             try
             {
-                
-                if (serialPort == null) return;
+                await ConnectToCOMPort();
 
-                // Disable the 'Connect' button 
-                comPortInput.IsEnabled = false;
+                _gpio = GpioController.GetDefault();
+                var pin4 = PrepareInputPin(4);
+                var pin18 = PrepareInputPin(18);
+                var pin17 = PrepareInputPin(17);
 
-                // Configure serial settings
-                serialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
-                serialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
-                serialPort.BaudRate = BAUD_RATE;
-                serialPort.Parity = SerialParity.None;
-                serialPort.StopBits = SerialStopBitCount.One;
-                serialPort.DataBits = 8;
-                serialPort.Handshake = SerialHandshake.None;
 
-                // Display configured settings
-                status.Text = "Serial port configured successfully: ";
-                status.Text += serialPort.BaudRate + "-";
-                status.Text += serialPort.DataBits + "-";
-                status.Text += serialPort.Parity.ToString() + "-";
-                status.Text += serialPort.StopBits;
+                msgQueue = new Queue<MidiMessage>();
 
-                // Set the RcvdText field to invoke the TextChanged callback
-                // The callback launches an async Read task to wait for data
-                rcvdText.Text = "Waiting for data...";
-
-                // Create cancellation token object to close I/O operations when closing the device
-                ReadCancellationTokenSource = new CancellationTokenSource();
-
-                // Enable 'WRITE' button to allow sending data
-                sendNoteButton.IsEnabled = true;
-
-                Listen();
-
+                timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromMilliseconds(10);
+                timer.Tick += Timer_Tick;
+                timer.Start();
             }
             catch (Exception ex)
             {
@@ -93,6 +80,67 @@ namespace RaspiMidiUwp
                 comPortInput.IsEnabled = true;
                 sendNoteButton.IsEnabled = false;
             }
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            timer.Stop();
+
+            if (msgQueue.Count > 0)
+            {
+                var msg = msgQueue.Dequeue();
+                if (msg != null)
+                {
+                    SendNote(msg.Note, msg.Velocity, msg.MsgChannel);
+                }
+            }
+
+            timer.Start();
+        }
+
+        private GpioPin PrepareInputPin(int pinNumber)
+        {
+            var pin = _gpio.OpenPin(pinNumber);
+            pin.DebounceTimeout = TimeSpan.FromTicks(0);
+            pin.ValueChanged += Pin_ValueChanged;
+
+            return pin;
+        }
+
+        private void Pin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+
+            uint msgChannel = args.Edge == GpioPinEdge.RisingEdge ? MIDI_NOTE_ON : MIDI_NOTE_OFF; // note on for channel 0 (MIDI #1)
+            uint velocity = 100;
+            uint note;
+
+            switch (sender.PinNumber)
+            {
+                case 4:
+                    note = 36;
+                    break;
+                case 17:
+                    note = 42;
+                    break;
+                case 18:
+                    note = 38;
+                    break;
+                default:
+                    note = 60; // middle C
+                    break;
+            }
+
+            //SendNote(note, velocity, msgChannel);
+            MidiMessage msg = new MidiMessage
+            {
+                Note = note,
+                Velocity = velocity,
+                MsgChannel = msgChannel
+            };
+
+            msgQueue.Enqueue(msg);
+
+            Debug.WriteLine(string.Format("Pin {0}: {1}", sender, args.Edge));
         }
 
         /// <summary>
@@ -113,19 +161,20 @@ namespace RaspiMidiUwp
         {
             DeviceInformation entry = _defaultDevice;
 
+            serialPort = await SerialDevice.FromIdAsync(_defaultDevice.Id);
+            if (serialPort == null) return;
+
             try
             {
-                serialPort = await SerialDevice.FromIdAsync(entry.Id);
                 if (serialPort == null) return;
 
                 // Disable the 'Connect' button 
                 comPortInput.IsEnabled = false;
 
-
                 // Configure serial settings
-                serialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+                serialPort.WriteTimeout = TimeSpan.FromMilliseconds(100);
                 serialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
-                serialPort.BaudRate = 57600;
+                serialPort.BaudRate = BAUD_RATE;
                 serialPort.Parity = SerialParity.None;
                 serialPort.StopBits = SerialStopBitCount.One;
                 serialPort.DataBits = 8;
@@ -140,7 +189,7 @@ namespace RaspiMidiUwp
 
                 // Set the RcvdText field to invoke the TextChanged callback
                 // The callback launches an async Read task to wait for data
-                rcvdText.Text = "Waiting for data...";
+                //rcvdText.Text = "Waiting for data...";
 
                 // Create cancellation token object to close I/O operations when closing the device
                 ReadCancellationTokenSource = new CancellationTokenSource();
@@ -148,7 +197,7 @@ namespace RaspiMidiUwp
                 // Enable 'WRITE' button to allow sending data
                 sendNoteButton.IsEnabled = true;
 
-                Listen();
+                //Listen();
             }
             catch (Exception ex)
             {
@@ -160,13 +209,19 @@ namespace RaspiMidiUwp
 
         /// <summary>
         /// sendTextButton_Click: Action to take when 'WRITE' button is clicked
-        /// - Create a DataWriter object with the OutputStream of the SerialDevice
-        /// - Create an async task that performs the write operation
+        /// - Send a note, velocity, and message/channel
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void sendNoteButton_Click(object sender, RoutedEventArgs e)
         {
+            SendNote(Convert.ToUInt32(txtNote.Text), Convert.ToUInt32(txtVelocity.Text), Convert.ToUInt32(txtChannel.Text));
+        }
+
+        private void SendNote(uint note, uint velocity, uint channel)
+        {
+            DataWriter dataWriteObject = null;
+
             try
             {
                 if (serialPort != null)
@@ -174,9 +229,12 @@ namespace RaspiMidiUwp
                     // Create the DataWriter object and attach to OutputStream
                     dataWriteObject = new DataWriter(serialPort.OutputStream);
 
-                    ////Launch the WriteAsync task to perform the write
-                    //await WriteAsync();
-                    SendNote(Convert.ToUInt32(txtNote.Text), Convert.ToUInt32(txtVelocity.Text), Convert.ToUInt32(txtChannel.Text));
+                    dataWriteObject.ByteOrder = ByteOrder.BigEndian;
+                    dataWriteObject.WriteByte(Convert.ToByte(note));
+                    dataWriteObject.WriteByte(Convert.ToByte(velocity));
+                    dataWriteObject.WriteByte(Convert.ToByte(channel));
+
+                    dataWriteObject.StoreAsync().AsTask();
                 }
                 else
                 {
@@ -187,159 +245,6 @@ namespace RaspiMidiUwp
             {
                 status.Text = this.Name + ": " + ex.Message;
             }
-            finally
-            {
-                // Cleanup once complete
-                if (dataWriteObject != null)
-                {
-                    dataWriteObject.DetachStream();
-                    dataWriteObject = null;
-                }
-            }
-        }
-
-        /// <summary>
-        /// WriteAsync: Task that asynchronously writes data from the input text box 'sendText' to the OutputStream 
-        /// </summary>
-        /// <returns></returns>
-        private async Task WriteAsync()
-        {
-            Task<UInt32> storeAsyncTask;
-
-            byte[] bytes = { };
-
-            // loop through and send the byte values of our message
-            dataWriteObject.ByteOrder = ByteOrder.BigEndian;
-
-            dataWriteObject.WriteByte(Convert.ToByte(36)); // note (60 = middle c)
-            dataWriteObject.WriteByte(Convert.ToByte(127)); // velocity (127 = loudest)
-            dataWriteObject.WriteByte(Convert.ToByte(144)); // note on channel 0
-
-            // Launch an async task to complete the write operation
-            storeAsyncTask = dataWriteObject.StoreAsync().AsTask();
-
-            UInt32 bytesWritten = await storeAsyncTask;
-            if (bytesWritten > 0)
-            {
-                //status.Text = sendText.Text + ", ";
-                //status.Text += "bytes written successfully!";
-            }
-        }
-
-        private void SendNote(uint note, uint velocity, uint channel, bool onOrOff = true)
-        {
-            dataWriteObject.ByteOrder = ByteOrder.BigEndian;
-
-            dataWriteObject.WriteByte(Convert.ToByte(note));
-            dataWriteObject.WriteByte(Convert.ToByte(velocity));
-            dataWriteObject.WriteByte(Convert.ToByte(channel));
-
-            // Launch an async task synchronously so we don't play one note before another
-            dataWriteObject.StoreAsync().AsTask().Wait();
-        }
-
-        /// <summary>
-        /// - Create a DataReader object
-        /// - Create an async task to read from the SerialDevice InputStream
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void Listen()
-        {
-            try
-            {
-                if (serialPort != null)
-                {
-                    dataReaderObject = new DataReader(serialPort.InputStream);
-
-                    // keep reading the serial input
-                    while (true)
-                    {
-                        await ReadAsync(ReadCancellationTokenSource.Token);
-                    }
-                }
-            }
-            catch (TaskCanceledException tce)
-            {
-                status.Text = "Reading task was cancelled, closing device and cleaning up";
-                CloseDevice();
-            }
-            catch (Exception ex)
-            {
-                status.Text = ex.Message;
-            }
-            finally
-            {
-                // Cleanup once complete
-                if (dataReaderObject != null)
-                {
-                    dataReaderObject.DetachStream();
-                    dataReaderObject = null;
-                }
-            }
-        }
-
-        /// <summary>
-        /// ReadAsync: Task that waits on data and reads asynchronously from the serial device InputStream
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        private async Task ReadAsync(CancellationToken cancellationToken)
-        {
-            Task<UInt32> loadAsyncTask;
-
-            uint ReadBufferLength = 1024;
-
-            // If task cancellation was requested, comply
-            cancellationToken.ThrowIfCancellationRequested();
-
-            // Set InputStreamOptions to complete the asynchronous read operation when one or more bytes is available
-            dataReaderObject.InputStreamOptions = InputStreamOptions.Partial;
-
-            using (var childCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken))
-            {
-                // Create a task object to wait for data on the serialPort.InputStream
-                loadAsyncTask = dataReaderObject.LoadAsync(ReadBufferLength).AsTask(childCancellationTokenSource.Token);
-
-                // Launch the task and wait
-                UInt32 bytesRead = await loadAsyncTask;
-                if (bytesRead > 0)
-                {
-                    rcvdText.Text = string.Empty;
-
-                    //rcvdText.Text = dataReaderObject.ReadString(bytesRead);
-                    byte[] bytes = { };
-                    var byteOrder = dataReaderObject.ByteOrder;
-
-                    // red them in reverse since it's BigEndian
-                    for (uint i = dataReaderObject.UnconsumedBufferLength; i > 0; i--)
-                    {
-                        rcvdText.Text += " " + dataReaderObject.ReadByte().ToString();
-                    }
-                    //rcvdText.Text = string.Format("byte count: {0}\r\n", bytes.Length);
-                    //foreach(var b in bytes)
-                    //{
-                    //    rcvdText.Text += Convert.ToString(b);
-                    //}
-
-                    status.Text = "bytes read successfully!";
-                }
-            }
-        }
-
-        /// <summary>
-        /// CancelReadTask:
-        /// - Uses the ReadCancellationTokenSource to cancel read operations
-        /// </summary>
-        private void CancelReadTask()
-        {
-            if (ReadCancellationTokenSource != null)
-            {
-                if (!ReadCancellationTokenSource.IsCancellationRequested)
-                {
-                    ReadCancellationTokenSource.Cancel();
-                }
-            }
         }
 
         /// <summary>
@@ -349,6 +254,8 @@ namespace RaspiMidiUwp
         /// </summary>
         private void CloseDevice()
         {
+            timer.Stop();
+
             if (serialPort != null)
             {
                 serialPort.Dispose();
@@ -374,7 +281,6 @@ namespace RaspiMidiUwp
             try
             {
                 status.Text = "";
-                CancelReadTask();
                 CloseDevice();
             }
             catch (Exception ex)
